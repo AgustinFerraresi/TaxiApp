aee4a7f08304a11e1723869eb443daea88819857
Agustin <agustinferraresi5@gmail.com>
2024-09-20 16:57:01 -0300

    pagina para registrarse

--- taxiAppProyect/src/App.jsx
+++ taxiAppProyect/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Register from './components/Register/Register'
+import "./App.css";
 
 function App() {
   
 
   return (
     <>
-
+    
+    
     </>
   )
 }
---
+++ taxiAppProyect/src/components/register/Register.jsx
@@ -0,0 +1,98 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import "./Register.css";
+import { useState } from 'react';
+
+
+function Register() {
+  const [userType,setUserType] = useState("");
+
+  const handleChange = (event) =>{
+    setUserType(event.target.value);
+    console.log("hola")
+  }
+
+
+
+  return (
+    <div id='form-container'>
+      
+      <Form>
+        <img src="./src/assets/logoTaxiApp.png" alt="logo"></img>
+        <h1>Crear usuario</h1>
+        
+        <div>
+          <label htmlFor="name">Nombre</label><br />
+          <input type="text" name="name" id="name" placeholder='Ingrese su nombre' />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label><br />
+          <input type="email" name="email" id="email" placeholder='Ingrese su email' required />
+        </div>
+
+        <div>
+          <label htmlFor="password">Contraseña</label><br />
+          <input type="password" name="password" id="password" placeholder='Ingrese su contraseña' required />
+        </div>
+
+        <div>
+          <label htmlFor="dni">DNI</label><br />
+          <input type="dni" name="dni" id="dni" placeholder='Ingrese su DNI' required />
+        </div>
+
+        
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check 
+          type="radio"
+          name='userType' 
+          value="passenger"  
+          className='userType' 
+          label="Soy pasajero"  
+          onChange={handleChange}
+          defaultChecked
+          />
+
+          <Form.Check 
+          type="radio" 
+          name='userType' 
+          value="taxi" 
+          className='userType'
+          id='taxi' 
+          label="Soy taxista" 
+          onChange={handleChange}
+          />
+        </Form.Group>
+
+        {
+          userType === "taxi" && 
+          <div>
+            <div>
+              <label htmlFor="car">Vehiculo</label><br />
+              <input type="car" name="car" id="car" placeholder='Ingrese su vehiculo' required />
+            </div>
+
+            <div>
+              <label htmlFor="car">Pantente</label><br />
+              <input type="car" name="car" id="car" placeholder='Ingrese la patente del taxi' required />
+            </div>
+          </div>
+
+          
+        }
+
+        
+          
+          
+        
+
+
+
+
+        <Button variant="warning" type="submit">Enviar</Button>
+      </Form>
+    </div>
+  );
+}
+
+export default Register;
