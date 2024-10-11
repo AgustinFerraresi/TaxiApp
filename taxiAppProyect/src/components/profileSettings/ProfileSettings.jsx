82747499ce077d0970f13176169be816df269974
Agustin <agustinferraresi5@gmail.com>
2024-10-11 03:16:13 -0300

    elimino comentarios

--- taxiAppProyect/src/components/profileSettings/ProfileSettings.jsx
+++ taxiAppProyect/src/components/profileSettings/ProfileSettings.jsx
@@ -1,51 +1,5 @@
-// import "./ProfileSettings.css";
-
-// const ProfileSettings = ({ user }) => {
-//     return (
-//         <div id="container">
-//             <form action="">
-//                 <div id="img-container">
-//                     <img src="./src/assets/fotoPerfilPrueba.png" id="profile-picture" alt="profile-picture" />
-//                     <h2>{user.name}</h2>
-//                 </div>
-
-//                 <label htmlFor="" className="general-info">Nombre
-//                     <input type="text" value={user.name} />
-//                 </label>
-
-//                 <label htmlFor="" className="general-info">Email
-//                     <input type="text" value={user.email} />
-//                 </label>
-
-//                 {
-//                     user.vehicles &&
-//                     <div>
-//                         <h3>Informacion del vehiculo</h3>
-//                         <label htmlFor="">Marca<br />
-//                             <input type="text" value={user.vehicles.brand} />
-//                         </label>
-
-//                         <label htmlFor="">Modelo<br />
-//                             <input type="text" value={user.vehicles.model} />
-//                         </label>
-
-//                         <label htmlFor="">Año<br />
-//                             <input type="text" value={user.vehicles.year} />
-//                         </label>
-//                     </div>
-//                 }
-//             </form>
-//         </div>
-//     );
-
-// };
-
-// export default ProfileSettings;
-
-// //
-
 import "./ProfileSettings.css";
-import { Button, Tab } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 
 const ProfileSettings = ({ user }) => {
