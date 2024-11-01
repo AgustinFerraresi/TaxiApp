595b7896a1eeff7b15d7650277fca768f951bc4f
mendieta967 <olorteguopedro@gmail.com>
2024-10-31 21:59:50 -0300

    themas

--- taxiAppProyect/src/components/navbar/Navbar.jsx
+++ taxiAppProyect/src/components/navbar/Navbar.jsx
@@ -1,30 +1,39 @@
-import "./Navbar.css"
+import { useContext } from "react";
+import "./Navbar.css";
+import { ThemeContext } from "../theme/ThemeContext";
+import { FaSun } from "react-icons/fa";
+import { MdNightlight } from "react-icons/md";
 
 const Navbar = () => {
-
-
-
-    return (
-        <div className="navbar-container">
-            <h2 className="text-warning" style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}>RoTaxi</h2>
-
-            <label class="theme-toggle">
-                <input type="checkbox" id="themeCheckbox" />
-                <span class="slider"></span>
-            </label>
-
-            <div class="language-selector">
-                <label for="languageSelect">Idioma:</label>
-                <select id="languageSelect">
-                    <option value="es">Español</option>
-                    <option value="en">English</option>
-                </select>
-            </div>
-
-
-        </div>
-    );
-}
-
+  const { theme, handleToggleTheme } = useContext(ThemeContext);
+  return (
+    <div className="navbar-container">
+      <h2
+        className="text-warning"
+        style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
+      >
+        RoTaxi
+      </h2>
+      <div className="Contenedor-iconos">
+        <button
+          className="theme-toggle"
+          onClick={() =>
+            handleToggleTheme(theme === "LIGHT" ? "DARK" : "LIGHT")
+          }
+          aria-label="Toggle theme"
+        >
+          {theme === "LIGHT" ? <FaSun /> : <MdNightlight />}
+        </button>
+      </div>
+      <div className="language-selector">
+        <label htmlFor="languageSelect"></label>
+        <select id="languageSelect">
+          <option value="es">Español</option>
+          <option value="en">English</option>
+        </select>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
---
+++ taxiAppProyect/src/components/theme/ThemeContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useEffect, useState } from "react";
+
+export const ThemeContext = createContext();
+
+const themeValue = localStorage.getItem("theme");
+
+export const ThemeContextProvider = ({ children }) => {
+  const [theme, setTheme] = useState(themeValue ?? "LIGHT");
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-bs-theme",
+      themeValue.toLocaleLowerCase()
+    );
+  }, []);
+
+  const handleToggleTheme = (newTheme) => {
+    if (newTheme === "DARK") {
+      document.documentElement.setAttribute("data-bs-theme", "dark");
+      localStorage.setItem("theme", newTheme);
+      setTheme(newTheme);
+    } else if (newTheme === "LIGHT") {
+      document.documentElement.setAttribute("data-bs-theme", "light");
+      localStorage.setItem("theme", newTheme);
+      setTheme(newTheme);
+    } else {
+      setTheme(newTheme);
+      localStorage.setItem("theme", newTheme);
+    }
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, handleToggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
